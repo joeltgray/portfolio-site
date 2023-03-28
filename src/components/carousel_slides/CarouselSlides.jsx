bd5e84320d07ead37409b3d993743d4e4d9c323c
Joel Gray <joeltgray@gmail.com>
2023-03-28 15:33:13 +0100

    Fix slide carousel

--- src/components/carousel_slides/CarouselSlides.jsx
+++ src/components/carousel_slides/CarouselSlides.jsx
@@ -1,24 +1,23 @@
 import React, { useEffect } from 'react';
 import './carouselSlides.css'
 import '../main/main.css'
-import blankautophoto from '../../assets/blankautophoto.jpg'
+import blankautophoto from '../../assets/blankautophoto.png'
 import graycode from '../../assets/graycode.jpg'
 import lwgrayart from '../../assets/lwgrayart.png'
 import pickapump from '../../assets/pickapump.png'
 import wizardingwords from '../../assets/wizardingwords.png'
 import Swiper, { Navigation, Pagination, Autoplay, EffectCoverflow } from 'swiper';
-
-
+import 'swiper/css';
+import 'swiper/css/pagination';
+import 'swiper/css/navigation';
 
 const websites = [
     { title: 'L.W.Gray Art', url: 'https://lwgrayart.com', image: lwgrayart, info: "A website I made for a family member which includes an image gallery, contact form and personal information to help display his work and allow potential customers to order commissioned personal artworks."},
     { title: 'Blank Auto Photo', url: 'https://blankautophoto.ie', image: blankautophoto, info: "An automotive photography blog I made for a friend. This website is wordpressed based but has had extensive custom styling to give the owner his desired look."},
     { title: 'Pick a Pump', url: 'https://pickapump.com', image: pickapump, info: "Pick a Pump is an app start up landing page I made, to display different features and potential business opportunities, includes custom contact form and FAQs for potential clients and investors."},
-    { title: 'Wizarding Words', url: 'https://wizardingwords.portkey.uk', image: wizardingwords, info: "Wizarding Words is a fun web wrapper written in React JS for a Harry Potter quote API I previously created."},
+    { title: 'Wizarding Words', url: 'https://wizardingwords.portkey.uk', image: wizardingwords, info: "Wizarding Words is a fun web wrapper written in React JS for a Harry Potter quote API that I previously created."},
     { title: 'Graycode', url: 'https://graycode.ie', image: graycode, info: "A small personal technology blog, this was the first website I made while in university. Created so I could share my interest and knowledge of web development, programming and engineering."},
 ];
-
-
 
 
 const CarouselSlides = () => {
@@ -27,25 +26,24 @@ const CarouselSlides = () => {
 
     useEffect(() => {
         new Swiper('#swiper-container', {
-          centeredSlides: true,
           slidesPerView: "auto",
           loop: true,
           effect: "coverflow",
-          coverflow: {
+          coverflowEffect: {
             rotate: 50,
             stretch: 0,
             depth: 100,
             modifier: 1,
-            slideShadows: false,
+            slideShadows: true,
           },
           autoplay: {
-            delay: 10000,
+            delay: 5000,
             disableOnInteraction: true
           },
           pagination: {
             el: '.swiper-pagination',
             clickable: true,
-          },
+          }
         });
       }, []);
     
@@ -65,10 +63,10 @@ const CarouselSlides = () => {
                                             <div className="row centered" style={{padding: "2%"}}> <p className="centered">{website.info}</p> <a className="links" href={website.url}>View the site</a></div>
                                         </div>
                                     ))}
+                                    
                             </div>
-                            
+                            <div className="swiper-pagination row centered"></div>
                         </div>
-                        <div className="swiper-pagination"></div>
                 </div>
             </div>
         </div>
