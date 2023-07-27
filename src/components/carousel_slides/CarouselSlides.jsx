f50d326ec4707344ffd3e1cf9163f976931165c7
Joel Gray <joeltgray@gmail.com>
2023-07-27 01:13:52 +0100

    Add github projects to site

--- src/components/carousel_slides/CarouselSlides.jsx
+++ src/components/carousel_slides/CarouselSlides.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect } from 'react';
-import { Header } from '../../containers';
 import './carouselSlides.css';
 import '../main/main.css';
+import '../../App.css';
 import blankautophoto from '../../assets/blankautophoto.png';
 import graycode from '../../assets/graycode.jpg';
 import lwgrayart from '../../assets/lwgrayart.png';
@@ -11,6 +11,39 @@ import Swiper, { Navigation, Pagination, Autoplay, EffectCoverflow } from 'swipe
 import 'swiper/css';
 import 'swiper/css/pagination';
 import 'swiper/css/navigation';
+
+const githubProjects = [
+    {
+        title: 'Odoo K8s',
+        description: 'A repo for building Odoo Community from source and deploying with microK8s on Linux.',
+        language: 'Shell',
+        url: 'https://github.com/joeltgray/odooK8s',
+        image: 'https://www.odoo.com/web/image/website/1/social_default_image?unique=5412cda'
+    },
+    {
+        title: 'Harry Potter API',
+        description: 'A rest API for retreiving Harry Potter quotes, users can GET a random quote or GET by UUID, this API was created using Python Flask and includes rating limiting.',
+        language: 'Python (Flask)',
+        url: 'https://github.com/joeltgray/HarryPotterAPI',
+        image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/3/3c/Flask_logo.svg/1200px-Flask_logo.svg.png'
+    },
+    {
+        title: 'AImaginary Creations',
+        description: 'An Instagram bot that uses random words, to generate a prompt for Open AI\'s Dalle-2. It creates a unique AI image in the style of a specific artist or art movement, then posts it to an instagram account. @AImaginary_Creations',
+        language: 'JavaScript',
+        url: 'https://github.com/joeltgray/AImaginaryCreations',
+        image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/4/4d/OpenAI_Logo.svg/2560px-OpenAI_Logo.svg.png'
+    },
+    {
+        title: 'Instagram Auto-follower',
+        description: 'A repo that allows users to search a hashtag on their feed, grab the posters primary key which is used to identify their profile uniquely, check their following status and automatically follow...',
+        language: 'JavaScript',
+        url: 'https://github.com/joeltgray/InstagramAutofollower',
+        image: 'https://adrienjoly.com/cours-javascript/js-logo.png'
+
+    },
+];
+
 
 const websites = [
     {
@@ -72,10 +105,25 @@ const CarouselSlides = () => {
     }, []);
 
     return (
-        <div id="page-wrapper">
-            <Header />
-            <div id="page-parent-content" className='page-parent-content'>
-                <h1>Previous Work</h1>
+        <>
+
+        <div className='portfolio-container'>        
+        <h2 className='self-align-left mt--heavy'>Github Projects</h2>
+        <p>Check out more on my<a href="https://github.com/joeltgray" target="_blank" rel="noopener noreferrer"> Github page</a></p>
+  
+                <div className="projects-container mt--heavy">
+                    {githubProjects.map((project, index) => (
+                        <div className="project-card" key={index}>
+                            <a href={project.url} target="_blank" rel="noopener noreferrer"><img src={project.image} alt={project.title} /></a>
+                            <h3><a href={project.url} target="_blank" rel="noopener noreferrer">{project.title}</a></h3>
+                            <p>{project.description}</p>
+                            <p>Language: {project.language}</p>
+                        </div>
+                    ))}
+                </div>
+
+                <div className="websites-container mt--super-heavy">
+                    <h2>Web Development</h2>
                         <div id="swiper-container" className="swiper-container">
                             <div className="swiper-wrapper">
                                 {websites.map((website, index) => (
@@ -103,6 +151,7 @@ const CarouselSlides = () => {
                         </div>
                     </div>
                 </div>
+                </>
     );
 };
 
--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -7,10 +7,15 @@ import '../../App.css'
 
 function Portfolio() {
     return (
-        <div>
+    <div id="page-wrapper">
+        <Header />
+        <div id="portfolio-page-parent-content">
+            <h1 className='self-align-center'>Portfolio</h1>
             <video autoPlay muted loop playsinline src={bgvideo} type="video/mp4" id="homepage-background" />
+
             <CarouselSlides />
         </div>
+    </div>
     );
 }
 
